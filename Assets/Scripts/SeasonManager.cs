775776ec7036fe89f16db4dde15af51ef5355c08
antoineG <antoine.gargasson@gmail.com>
2021-12-14 19:56:07 +0100

    Added season System

---
+++ Assets/Scripts/SeasonManager.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum Season
+{
+    SPRING,
+    SUMMER,
+    AUTOMN,
+    WINTER
+}
+
+public class SeasonManager : MonoBehaviour
+{
+    public static SeasonManager instance = null;
+
+    private float currentTime = 0;
+    private float seasonSpeed = 1;
+
+    private Season currentSeason = Season.SPRING;
+
+    public Text seasonText = null;
+    public Slider seasonSlider = null;
+
+    private float seasonLegth = 30;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Update()
+    {
+        currentTime += Time.deltaTime * seasonSpeed;
+        seasonSlider.value = currentTime / seasonLegth;
+        if (currentTime >= seasonLegth)
+        {
+            currentTime = 0;
+            switch(currentSeason)
+            {
+                case Season.SPRING:
+                    {
+                        currentSeason = Season.SUMMER;
+                        break;
+                    }
+                case Season.SUMMER:
+                    {
+                        currentSeason = Season.AUTOMN;
+                        break;
+                    }
+                case Season.AUTOMN:
+                    {
+                        currentSeason = Season.WINTER;
+                        break;
+                    }
+                case Season.WINTER:
+                    {
+                        currentSeason = Season.SPRING;
+                        break;
+                    }
+            }
+            seasonText.text = "Season : " + currentSeason;
+        }
+    }
+}
